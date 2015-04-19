6b6603e78bf05ccc77ba6763ef345bebb2aedb79
kuilin <wklglider@gmail.com>
2015-04-19 13:59:18 -0600

    Merge branch 'Kuilin'
    Conflicts:
    	app/src/main/java/csci3320/kuilin/sat/gameActivity.java
    	app/src/main/java/csci3320/kuilin/sat/welcomeActivity.java

--- app/src/main/java/csci3320/kuilin/sat/DatabaseOperations.java
+++ app/src/main/java/csci3320/kuilin/sat/DatabaseOperations.java
@@ -2,41 +2,84 @@ package csci3320.kuilin.sat;
 
 import android.content.ContentValues;
 import android.content.Context;
+import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
 import android.util.Log;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * Created by kuilin on 4/19/15.
  */
 public class DatabaseOperations extends SQLiteOpenHelper {
+    // All Static variables
+    // Database Version
     public static final int database_version = 1;
-    public String CREATE_QUERY = "CREATE TABLE " + TableData.TableInfo.TABLE_NAME + "(" + TableData.TableInfo.USER_NAME + " TEXT," + TableData.TableInfo.USER_SCORE + " TEXT );";
+    // Score Table Columns names
+    public static final String USER_NAME = "user_name";
+    public static final String USER_SCORE = "user_score";
+    // Database Name
+    public static final String DATABASE_NAME = "user_info";
+    // Score table name
+    public static final String TABLE_NAME = "reg_info";
 
-    public DatabaseOperations(Context context, String name, SQLiteDatabase.CursorFactory factory, int version) {
-        super(context, TableData.TableInfo.DATABASE_NAME, null, database_version);
+    public DatabaseOperations(Context context) {
+        super(context, DATABASE_NAME, null, database_version);
         Log.d("Database operations", "Database created");
     }
 
+    // Creating Tables
     @Override
     public void onCreate(SQLiteDatabase db) {
-        db.execSQL(CREATE_QUERY);
+        String CREATE_SCORE_TABLE = "CREATE TABLE " + TABLE_NAME + "(" + USER_NAME + " TEXT," + USER_SCORE + " TEXT );";
+        db.execSQL(CREATE_SCORE_TABLE);
         Log.d("Database operations", "Table created");
 
     }
 
+    // Upgrading database
     @Override
     public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+        // Drop older table if existed
+        db.execSQL("Drop table if exists " + TABLE_NAME);
+        // Create tables again
+        onCreate(db);
 
     }
 
-    public void putInfomation(DatabaseOperations dop, String name, String score) {
-        SQLiteDatabase sql = dop.getWritableDatabase();
+    // All Operations (Create, Read, Update, Delete)
+    // Adding new user information
+    public void putInformation(String name, String score) {
+        SQLiteDatabase sql = this.getWritableDatabase();
         ContentValues cv = new ContentValues();
-        cv.put(TableData.TableInfo.USER_NAME, name);
-        cv.put(TableData.TableInfo.USER_SCORE, score);
-        long k = sql.insert(TableData.TableInfo.TABLE_NAME, null, cv);
-        Log.d("Database operations", "One row inserted");
+        cv.put(USER_NAME, name); // User name
+        cv.put(USER_SCORE, score); // User score
+        // Inserting row
+        sql.insert(TABLE_NAME, null, cv);
+        // Closing database connection
+        close();
 
+    }
+
+    // Getting all user information
+    public List getInformation(String name, String score) {
+        List scoreList = new ArrayList();
+        // Select all query
+        String selectQuery = "Select from " + TABLE_NAME;
+        SQLiteDatabase sql = this.getReadableDatabase();
+        Cursor cursor = sql.rawQuery(selectQuery, null);
+        // Looping through all rows and adding to list
+        if (cursor.moveToFirst()) {
+            do {
+                // Adding contact to list
+                scoreList.add(cursor.getString(0));
+                scoreList.add(cursor.getString(1));
+            } while (cursor.moveToNext());
+        }
+
+        // return score list
+        return scoreList;
     }
 }
