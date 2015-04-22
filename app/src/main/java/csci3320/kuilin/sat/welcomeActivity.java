75ae08d20d14f1da49249c1d9c9ce3bd765b824b
Kirk Karavouzis <kirk.karavouzis@ucdenver.edu>
2015-04-22 07:31:41 -0600

    cleaned up code.  added strings in strings.xml

--- app/src/main/java/csci3320/kuilin/sat/welcomeActivity.java
+++ app/src/main/java/csci3320/kuilin/sat/welcomeActivity.java
@@ -11,7 +11,6 @@ import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
-import android.widget.RadioButton;
 import android.widget.RadioGroup;
 import android.widget.TextView;
 
